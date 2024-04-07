c5f926b552e08f1a6a83e999ed1c4eccb05bf2c0
Evgenii Surikov <surikov96@mail.ru>
2024-04-07 20:58:52 +0700

    Add code

--- init.sql
+++ init.sql
@@ -1,2 +1,17 @@
-CREATE TABLE studens (id int PRIMARY KEY GENERATED ALWAYS AS IDENTITY, firstname varchar(250), lastname varchar(250), evaluation int);
+CREATE TABLE students (id int PRIMARY KEY GENERATED ALWAYS AS IDENTITY, firstname varchar(250), lastname varchar(250), evaluation int);
+
+INSERT INTO students(firstname, lastname, evaluation) VALUES ('','',100);
+INSERT INTO students(firstname, lastname, evaluation) VALUES ('','',100);
+INSERT INTO students(firstname, lastname, evaluation) VALUES ('','',95);
+INSERT INTO students(firstname, lastname, evaluation) VALUES ('','',89);
+INSERT INTO students(firstname, lastname, evaluation) VALUES ('','',80);
+INSERT INTO students(firstname, lastname, evaluation) VALUES ('','',80);
+INSERT INTO students(firstname, lastname, evaluation) VALUES ('','',77);
+INSERT INTO students(firstname, lastname, evaluation) VALUES ('','',75);
+INSERT INTO students(firstname, lastname, evaluation) VALUES ('','',75);
+INSERT INTO students(firstname, lastname, evaluation) VALUES ('','',65);
+INSERT INTO students(firstname, lastname, evaluation) VALUES ('','',50);
+INSERT INTO students(firstname, lastname, evaluation) VALUES ('','',45);
+
+
 
--- src/main/java/org/surikov/homework10/dao/Dao.java
+++ src/main/java/org/surikov/homework10/dao/Dao.java
@@ -1,20 +1,30 @@
 package org.surikov.homework10.dao;
 
-import com.sun.jdi.connect.spi.Connection;
+import org.surikov.homework10.entity.Studens;
 
-import java.util.HashMap;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 
 public class Dao {
-    DBManager dbManager;
-    Connection connection;
 
-    public Map getResult(){
+    public List getResult(){
 
-        Map<String,String> map = new HashMap<>();
+        List<Studens> studensList = new ArrayList<>();
 
+        try(Connection connection = DBManager.createConnection()) {
+            String query = "SELECT * FROM students";
+            PreparedStatement preparedStatement = connection.prepareStatement(query);
+            ResultSet resultSet = preparedStatement.getResultSet();
 
-        return
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return studensList;
     }
 
 }
